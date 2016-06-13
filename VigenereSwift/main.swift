e933e3830a80ebe183f9b96cce7f3acb4dd2b18a
James Goldstein <jamesdylangoldstein@gmail.com>
2016-06-13 12:12:08 -0700

    Initial Commit

---
+++ VigenereSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VigenereSwift
+//
+//  Created by James Dylan Goldstein on 6/13/16.
+//  Copyright © 2016 James Dylan Goldstein. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
